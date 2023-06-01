cbe4e0ab7a2c1386678c9cc3e508524d970f0b7b
kim-yujeong <dbwjdeor1234562@gmail.com>
2023-06-01 16:07:31 +0900

    0531 회원 이메일 검색 완료

--- StratoSts/src/main/java/com/strato/skylift/member/repository/MyPageRepository.java
+++ StratoSts/src/main/java/com/strato/skylift/member/repository/MyPageRepository.java
@@ -5,11 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 @Repository
 public interface MyPageRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByMemberCode(@Param("memberCode") Long memberCode);
 
-    Member findByMemberName(String name);
+//    Member findByMemberName(String name);
+
+    List<Member> findAllByMemberName(String name);
 }
